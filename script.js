d8139fbd4eac63bc6ef176ada397f158067cda3e
Zumbua <flpyounan@yahoo.com.br>
2023-06-04 15:12:32 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "foto de Mayk Brito sorrindo, usando óculos escuros e camiseta e jaqueta de couro preta, barba e fundo amarelo"
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "foto de Mayk Brito sorrindo, usando óculos e camiseta preta, barba e fundo amarelo"
+    )
+  }
+}
