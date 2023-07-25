449855d647cb79a1d76d7ea80bbce0cf9f23d320
Zumbua <flpyounan@yahoo.com.br>
2023-07-25 17:16:20 -0300

    ajustes para apresentação

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/fyounan.png")
     img.setAttribute(
       "alt",
       "foto de Mayk Brito sorrindo, usando óculos escuros e camiseta e jaqueta de couro preta, barba e fundo amarelo"
     )
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/fyounan.png")
     img.setAttribute(
       "alt",
       "foto de Mayk Brito sorrindo, usando óculos e camiseta preta, barba e fundo amarelo"
